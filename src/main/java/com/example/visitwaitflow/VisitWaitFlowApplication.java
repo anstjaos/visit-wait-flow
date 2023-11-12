fa26e4e1dbc1060ef823ecff6ee6fb5eeefced9a
anstjaos <vv_eros@naver.com>
2023-11-12 21:10:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/visitwaitflow/VisitWaitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitwaitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitWaitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitWaitFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visitwaitflow/VisitWaitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visitwaitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitWaitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
